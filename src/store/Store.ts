7db57a4aae9cfce2e3212c38d9a445df4a66e54d
rohiitgit <rohitcodes03@gmail.com>
2024-08-01 20:07:19 +0530

    init store

---
+++ src/store/Store.ts
@@ -0,0 +1,27 @@
+export type UserId = string;
+
+export interface Chat{
+    id: string;
+    userId: UserId;
+    name: string;
+    message: string;
+    upvotes: UserId[]; //who has upvoted what
+}
+
+export abstract class Store {
+    constructor() {
+
+    }
+    initRoom(roomId: string){
+
+    }
+    getChats(room: string, limit: number, offset: number){
+
+    }
+    addChat(userId: UserId, name: string, room: string, message: string){
+
+    }
+    upvote(userId: UserId, room: string, chatId: string){
+
+    }
+}
